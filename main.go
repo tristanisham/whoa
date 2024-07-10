fd29b2ae843c8cdd9abc1c34b4c133f99d783c35
Tristan Isham <tristan@isham.co>
2024-07-09 22:06:25 -0400

    updated CLI

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/charmbracelet/log"
 	"net/http"
 	"os"
+	"path/filepath"
+
+	"github.com/charmbracelet/log"
 
 	"github.com/dunglas/frankenphp"
 	opts "github.com/urfave/cli/v2"
@@ -18,31 +20,77 @@ var app = &opts.App{
 	Version: VERSION,
 	Copyright: "© 2024 Tristan Isham",
 	Suggest: true,
+	Before: func(ctx *opts.Context) error {
+		if _, ok := os.LookupEnv("WHOA_DEBUG"); ok {
+			log.SetLevel(log.DebugLevel)
+		}
+		return nil
+	},
+	Commands: []*opts.Command{
+		{
+			Name: "run",
+			Usage: "Run a PHP script from the command line",
+			Aliases: []string{"r"},
+			Description: "Run a PHP script as a PHP command line script",
+			Args: true,
+			ArgsUsage: " <script.php>",
+			Action: func(ctx *opts.Context) error {
+				script := ctx.Args().First()
+				os.Exit(frankenphp.ExecuteScriptCLI(script, ctx.Args().Slice()))
+				return nil
+			},
+		},
+		{
+			Name: "fs",
+			Usage: "Start a PHP file-based server",
+			Aliases: []string{"r"},
+			Description: "Start a traditional PHP server with file-based routing",
+			Args: true,
+			ArgsUsage: " </path/to/dir>",
+			Flags: []opts.Flag{
+				&opts.BoolFlag{
+					Name: "sym",
+					Usage: "Enable symblinks (may generate errors due to caching if symlinks are changed and Whoa isn't restarted)",
+				},
+			},
+			Action: func(ctx *opts.Context) error {
+				dir := ctx.Args().First()
+
+				if err := frankenphp.Init(); err != nil {
+					panic(err)
+				}
+				defer frankenphp.Shutdown()
+
+				target := dir
+				if newTarget, err := filepath.Abs(dir); err == nil {
+					target = newTarget
+				}
+			
+				http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+					req, err := frankenphp.NewRequestWithContext(r, frankenphp.WithRequestDocumentRoot(target, ctx.Bool("sym")))
+					if err != nil {
+						panic(err)
+					}
+			
+					if err := frankenphp.ServeHTTP(w, req); err != nil {
+						panic(err)
+					}
+				})
+				if err := http.ListenAndServe(":8080", nil); err != nil {
+					return err
+				}
+
+				return nil
+			},
+		},
+	},
 }
 
 func main() {
-	if _, ok := os.LookupEnv("WHOA_DEBUG"); ok {
-		log.SetLevel(log.DebugLevel)
-	}
-
+	
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
 
-	if err := frankenphp.Init(); err != nil {
-		panic(err)
-	}
-	defer frankenphp.Shutdown()
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		req, err := frankenphp.NewRequestWithContext(r, frankenphp.WithRequestDocumentRoot("./public", false))
-		if err != nil {
-			panic(err)
-		}
-
-		if err := frankenphp.ServeHTTP(w, req); err != nil {
-			panic(err)
-		}
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	
 }
